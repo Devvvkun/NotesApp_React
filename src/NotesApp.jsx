9f71327aa4fb85524e7009355a4565a153a70c78
Devvvkun <himanshusinghsvmsln@gmail.com>
2026-09-03 01:24:45 +0530

    Notes UI fixed

--- src/NotesApp.jsx
+++ src/NotesApp.jsx
@@ -3,13 +3,20 @@ import React, {useState} from 'react'
 const NotesApp = () => {
     const [Title, setTitle] = useState("")
     const [sbtTitle, setsbtTitle] = useState("")
-    const handleInput = (e) => {
+    const [Desc, setDesc] = useState("")
+    const [sbtDesc, setsbtDesc] = useState("")
+    const handleTitle = (e) => {
         setTitle(e.target.value)
+    }
+    const handleDesc = (e) => {
+        setDesc(e.target.value)
     }
      const handleSubmit = (e) =>{
             e.preventDefault()
             setsbtTitle(Title)
+            setsbtDesc(Desc)
             setTitle("")
+            setDesc("")
         }
   return (
   <form onSubmit={handleSubmit}>  <div className='flex flex-col w-full sm:flex-row'>
@@ -17,8 +24,8 @@ const NotesApp = () => {
         <div className="addNotes flex w-full flex-col overflow-hidden sm:w-1/2">
         <h3 className='m-2'>Add Notes:</h3>
         
-            <input type="text" name="title" placeholder='Enter your Tasks here' onChange={handleInput} value = {Title} className='border-2 outline-none px-4 py-2 m-3'/>
-            <input className=' border-2 outline-none px-4 pt-0 py-20  m-3' type="text" name="desc" placeholder='Enter your task description'/>
+            <input type="text" name="title" placeholder='Enter your Tasks here' onChange={handleTitle} value = {Title} className='border-2 outline-none px-4 py-2 m-3'/>
+            <input className=' border-2 outline-none px-4 pt-0 py-20  m-3' type="text" name="desc" onChange={handleDesc} value={Desc} placeholder='Enter your task description'/>
            <button
   type="submit"
   className="rounded bg-white font-medium text-black px-4 py-2 m-3"
@@ -28,7 +35,7 @@ const NotesApp = () => {
        
         </div>
         <div className="shownotes  h-screen w-1/2 flex flex-wrap my-0.5 overflow-hidden sm:border-l-2 sm:border-white">
-            <div className="note h-60 w-[45%] mx-2 my-2 lg:w-[27%] bg-white text-black">{sbtTitle}</div>
+            <div className="note h-60 w-[45%] mx-2 my-2 lg:w-[27%] bg-white text-black p-4  wrap-break-word"><h1 className='font-medium'>{sbtTitle}</h1> <br /> <p className='pt-2 '>{sbtDesc}</p></div>
            
         </div>
     </div> </form>
